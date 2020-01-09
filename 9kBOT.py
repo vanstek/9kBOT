994f6bd10e2bb8e5b199f7b7ee6ec876f8301e64
Alex van Stekelenborg <35575349+AlStek@users.noreply.github.com>
2020-01-09 12:18:21 -0500

    created script file
    downloaded praw

---
+++ 9kBOT.py
@@ -0,0 +1,11 @@
+
+import praw
+import pdb
+import re
+import os
+
+reddit = praw.Reddit('9kbabyyyyyyyyyyyyy')
+
+bitcoinsub = reddit.subreddit("bitcoin")
+cryptosub = reddit.subreddit("cryptoCurrency") d
+

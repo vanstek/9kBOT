9860c3ecbf2b719c9519bcbb471235ff282f421e
Alex van Stekelenborg <35575349+AlStek@users.noreply.github.com>
2020-01-09 12:38:54 -0500

    added crontab miniguide

--- 9kBOT.py
+++ 9kBOT.py
@@ -4,8 +4,10 @@ import pdb
 import re
 import os
 
+#selects user account. password is in praw.ini file
 reddit = praw.Reddit('9kbabyyyyyyyyyyyyy')
 
+#subreddits to post to
 bitcoinsub = reddit.subreddit("bitcoin")
-cryptosub = reddit.subreddit("cryptoCurrency") d
+cryptosub = reddit.subreddit("cryptoCurrency")
 
